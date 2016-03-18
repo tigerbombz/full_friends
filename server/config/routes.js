71aae6c9c95d3d73d8de6e925efbe90e5ae5d93b
tigerbombz <madd1778@yahoo.com>
2016-03-17 20:21:49 -0700

    init repo for adding friends in MEAN

---
+++ server/config/routes.js
@@ -0,0 +1,24 @@
+var friends = require('./../controllers/friends.js');
+
+  // This is our routes.js file located in server/config/routes.js
+  // This is where we will define all of our routing rules!
+  // We will have to require this in the server.js file (and pass it app!)
+  module.exports = function(app) {
+  // verb: get, plural of target as the URI is the RESTful index method (it returns all friends)
+
+    app.get('/friends', function(req, res) {
+      console.log("Im in my routes GET");
+      friends.show(req, res);
+    })
+
+    app.post('/friends', function(req, res){
+      console.log("im in my routes POST");
+      friends.post(req, res);
+    });
+
+    app.delete('/friends/:id', function(req, res){
+      console.log("Trying to Delete");
+      friends.delete(req, res);
+    })
+
+  };
---
+++ server/controllers/friends.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+var Friend = mongoose.model('Friend');
+
+module.exports = (function() {
+  return {
+    show: function(req, res) {
+      Friend.find({}, function(err, results){
+        if(err) {
+          console.log(err);
+        } else {
+          res.json(results);
+        }
+      });
+    },
+
+    post: function(req, res) {
+      console.log("im in my controllers");
+      Friend.create(req.body, function(err, results){
+        if(err) {
+          console.log(err);
+        } else {
+          res.json(results);
+        }
+      });
+    },
+
+    delete: function(req, res){
+
+    }
+
+  };
+})();
+
